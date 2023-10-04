e2d9d2961dbaec46eb6220c658d48f5b831461a0
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-10-04 18:37:11 +0900

    -

---
+++ png.mjs
@@ -0,0 +1,8 @@
+const// https://qiita.com/McbeEringi/items/9928a9bc05798924e68c
+png=({width:w,height:h,alpha:a,palette:p,data:d})=>((
+	
+)=>[
+	137,80,78,71,13,10,26,10,// header
+])();
+
+export{png};
