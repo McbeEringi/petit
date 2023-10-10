cb510607938703953da2a541742fe04e01c36bd2
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2023-10-10 20:58:22 +0900

    -

--- png.mjs
+++ png.mjs
@@ -1,8 +1,13 @@
 const// https://qiita.com/McbeEringi/items/9928a9bc05798924e68c
-png=({width:w,height:h,alpha:a,palette:p,data:d})=>((
-	
-)=>[
+png=({data:d,width:w,height:h,palette:p})=>((
+	crc=(t=>(buf,crc=0)=>~buf.reduce((c,x)=>t[(c^x)&0xff]^(c>>>8),~crc))([...Array(256)].map((_,n)=>[...Array(8)].reduce(c=>(c&1)?0xedb88320^(c>>>1):c>>>1,n))),// https://www.rfc-editor.org/rfc/rfc1952
+	adler=data=>{let a=1,b=0,len=data.length,tlen,i=0;while(len>0){len-=(tlen=Math.min(1024,len));do{b+=(a+=data[i++]);}while(--tlen);a%=65521;b%=65521;}return(b<<16)|a;},
+	be4=x=>[x>>>24&255,x>>>16&255,x>>>8&255,x>>>0&255],ch=x=>[...be4(x.length-4),...x,...be4(crc(x))],
+)=>({data:[
 	137,80,78,71,13,10,26,10,// header
-])();
+	...ch([73,72,68,82, ...be4(w),...be4(h), p?(p.length):8,p?3:alpha?6:1, 0,0,0]),// IHDR: w h bitDepth colType 0,0,0
+	...(p?ch():[]),
+	0,0,0,0,73,69,78,68,174,66,96,130// IEND
+],toDataURL(){return'data:image/png;base64,'+btoa(String.fromCharCode(...this.data));},toBlob(){return new Blob([new Uint8Array(this.data)],{type:'image/png'})}}))();
 
 export{png};
