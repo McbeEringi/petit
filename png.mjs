69579489cb17ae2cc016a9131e42fe5bc4d15b53
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-10-10 22:29:47 +0900

    png_split_zlib_block

--- png.mjs
+++ png.mjs
@@ -8,7 +8,7 @@ png=({data:d,width:w,height:h,palette:p,alpha:a})=>((
 	...ch([73,72,68,82, ...be4(w),...be4(h), bd,p?3:alpha?6:2, 0,0,0]),// IHDR: w h bitDepth colType 0,0,0
 	...p?ch([80,76,84,69,...p.flatMap(x=>be4(a?x>>>8:x).slice(1))]):[],// PLTE: ...RGB
 	...p&&a?ch([116,82,78,83,...p.map(x=>x&255)]):[],// tRNS: ...alpha
-	...ch([73,68,65,84, 8,29,1, ...((x,l=x.length)=>[l>>>0&255,l>>>8&255,~l>>>0&255,~l>>>8&255,...x,...be4(adler(x))])(
+	...ch([73,68,65,84, 8,29, ...(x=>[...[...Array(Math.ceil(x.length/65535))].flatMap((y,i,{length:a},l)=>(y=x.slice(65535*i++,65535*i),l=y.length,[i==a,l>>>0&255,l>>>8&255,~l>>>0&255,~l>>>8&255,...y])),...be4(adler(x))])(
 		[...Array(h)].flatMap((_,i)=>(i=d.slice(w*i,w*++i),[0,...p?[...Array(Math.ceil(w/bdi))].map((_,j)=>[...Object.assign(i.slice(bdi*j,bdi*++j),{length:bdi})].reduce((a,x)=>a<<bd|(x&2**bd-1),0)):i]))
 	)]),// DATA
 	0,0,0,0,73,69,78,68,174,66,96,130// IEND
