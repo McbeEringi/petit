7096fe53a609ac37621014627bd79dd169e075af
McbeEringi <git@mcbeeringi.dev>
2024-11-17 03:48:40 +0900

    I_AM_UNIX_ZIP

--- zip.mjs
+++ zip.mjs
@@ -1,13 +1,13 @@
 const
-zip=(w=[],f=_=>_,cs)=>((
-	u=x=>new Uint8Array(x),zz=u([0,0],cs=cs&&self.CompressionStream),vz=u([cs?20:10,0]),pk=u([80,75]),_12=u([1,2]),_34=u([3,4]),gf=u([0,8]),cm=cs?u([8,0]):zz,
+zip=(w=[],f=_=>_,cs)=>((// Version Made by: UNIX cf.ExtFileAttrHIGH...<bits/stat.h> le2(reguler644=0x81a4)=ea
+	u=x=>new Uint8Array(x),zz=u([0,0],cs=cs&&self.CompressionStream),vr=u([cs?20:10,0]),vm=u([20,3]),ea=u([164,129]),pk=u([80,75]),_12=u([1,2]),_34=u([3,4]),gf=u([0,8]),cm=cs?u([8,0]):zz,
 	le2=x=>u([x,x>>>8]),le4=x=>u([x,x>>>8,x>>>16,x>>>24]),l=x=>x.byteLength||x.size||0,cnt=x=>le4(x.reduce((a,y)=>a+l(y),0)),te=new TextEncoder(),i=0,
 	iab=x=>x instanceof ArrayBuffer,dfl=b=>cs?new Response((iab(b)?new Blob([b]):b).stream().pipeThrough(new cs('deflate-raw'))).blob():b,
 	ddt=x=>((x.getFullYear()-1980)<<25)|((x.getMonth()+1)<<21)|(x.getDate()<<16)|(x.getHours()<<11)|(x.getMinutes()<<5)|(x.getSeconds()>>1),// mmmsssss hhhhhmmm MMMDDDDD YYYYYYYM // Y-=1980;s/=2;
 	crc=(t=>(buf,crc=0)=>~buf.reduce((c,x)=>t[(c^x)&0xff]^(c>>>8),~crc))([...Array(256)].map((_,n)=>[...Array(8)].reduce(c=>(c&1)?0xedb88320^(c>>>1):c>>>1,n)))// https://www.rfc-editor.org/rfc/rfc1952
 )=>w.reduce(async(a,x,b,cb,n)=>(cb=await dfl(b=x.buffer||x),f(++i/w.length/3),n=te.encode(x.name),
-	x=[vz,gf,cm,le4(ddt(new Date(x.lastModified||Date.now()))),le4(crc(u(iab(b)?b:await new Response(b).arrayBuffer()))),le4(l(cb)),le4(l(b)),le2(l(n)),zz],// vReq flag cpsType date CRC32 cpsSize rawSize nameLength extLength
-	f(++i/w.length/3),a=await a,f(++i/w.length/3),a.cd.push(pk,_12,vz,...x,zz,zz,zz,zz,zz,cnt(a.lf),n),a.lf.push(pk,_34,...x,n,cb),a// PK0102 vMade x cmtLength 0304disk intAttr extAttrLSB extAttrMSB 0304pos name , PK0304 x name content
+	x=[vr,gf,cm,le4(ddt(new Date(x.lastModified||Date.now()))),le4(crc(u(iab(b)?b:await new Response(b).arrayBuffer()))),le4(l(cb)),le4(l(b)),le2(l(n)),zz],// vReq flag cpsType date CRC32 cpsSize rawSize nameLength extLength
+	f(++i/w.length/3),a=await a,f(++i/w.length/3),a.cd.push(pk,_12,vm,...x,zz,zz,zz,zz,ea,cnt(a.lf),n),a.lf.push(pk,_34,...x,n,cb),a// PK0102 vMade x cmtLength 0304disk intAttr extAttrLSB extAttrMSB 0304pos name , PK0304 x name content
 ),{lf:[],cd:[]}).then((x,_=le2(w.length))=>new Blob([...x.lf,...x.cd,pk,u([5,6]),zz,zz,_,_,cnt(x.cd),cnt(x.lf),zz],{type:'application/zip'})))(),// PK0506 disk 0304startDisk cnt0102disk cnt0102all 0102size 0102pos cmtLength
 
 unzip=async(w=new Blob())=>((
