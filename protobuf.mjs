408060b580aa799ff91237791267963f9f6f5a46
McbeEringi <git@mcbeeringi.dev>
2024-11-27 23:59:49 +0900

    -

---
+++ protobuf.mjs
@@ -0,0 +1,19 @@
+const
+depb=w=>(console.log(w),[...{[Symbol.iterator]:(
+	p=0,
+	state=0,
+	head,
+	val=_=>[...{[Symbol.iterator]:d=>({next:_=>({done:d,value:d||(d=!(w[p]&0x80),w[p++]&0x7f)})})}].reduce((a,x,i)=>x<<(7*i)|a)
+)=>({next:_=>w.length<=p?{done:1}:{value:(
+	(([i,x])=>({i,type:x,value:[
+		_=>val(),
+		_=>w.slice(p,p+=8),
+		(l=val())=>w.slice(p,p+=l),
+		_=>_,
+		_=>_,
+		_=>w.slice(p,p+=4),
+	][x]()}))((x=>[x>>>3,x&7])(val()))
+)}})}]),
+enpb=1;
+
+export{depb,enpb};
--- totp.mjs
+++ totp.mjs
@@ -1,5 +1,6 @@
+import{depb}from'./protobuf.mjs';
 const
-totp=async({date:t=new Date,algorithm:h='SHA1',digits:l=6,period:d=30,secret:k,_:{
+totp=async({date:t=new Date,algorithm:h='SHA-1',digits:l=6,period:d=30,secret:k='',_:{
 	be4=x=>[x>>>24&255,x>>>16&255,x>>>8&255,x>>>0&255],
 	b32de=w=>((
 		d=[...'abcdefghijklmnopqrstuvwxyz234567'].reduce((a,x,i)=>(a[x]=i.toString(2).padStart(5,0),a),{}),
@@ -10,13 +11,23 @@ totp=async({date:t=new Date,algorithm:h='SHA1',digits:l=6,period:d=30,secret:k,_
 		await crypto.subtle.importKey('raw',b32de(k),{name:'HMAC',hash:h},0,['sign']),
 		new Uint8Array([...be4((t=t.getTime()/1000/d)/2**32),...be4(t)])
 	))]
-);
+),
+migurl=w=>depb(new TextEncoder().encode(atob(decodeURIComponent(new URLSearchParams(new URL(w).search).get('data'))))).map(x=>(
+	x.i==1&&(x=depb(x.value)),x
+));
 
 export{totp};
 
-// console.log(
-// 	new Date(),
-// 	await totp({
-// 		secret:'helloworld234567',
-// 	})
-// );
+console.log(
+	new Date(),
+	await totp({
+		secret:'helloworld234567',
+	})
+);
+const url='otpauth-migration://offline?data=CiQKCjkWt1nRWPW%2Bd98SEGhlbGxvd29ybGQyMzQ1NjcgASgBMAIQAhgBIAA%3D';
+console.log(
+	migurl(url)
+);
+
+
+
