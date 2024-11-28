8c41f5d98a58fcf287ef01523d8607f56095bac6
McbeEringi <git@mcbeeringi.dev>
2024-11-28 23:19:47 +0900

    -

--- totp.mjs
+++ totp.mjs
@@ -1,14 +1,17 @@
 import{depb}from'./protobuf.mjs';
 const
-totp=async({date:t=new Date,algorithm:h='SHA-1',digits:l=6,period:d=30,secret:k='',_:{
-	be4=x=>[x>>>24&255,x>>>16&255,x>>>8&255,x>>>0&255],
-	b32de=w=>((
-		d=[...'abcdefghijklmnopqrstuvwxyz234567'].reduce((a,x,i)=>(a[x]=i.toString(2).padStart(5,0),a),{}),
-		b=w.toLowerCase().replace(/./g,x=>d[x]||'')
-	)=>new Uint8Array([...Array(Math.ceil(b.length/8))].map((_,i)=>+`0b${b.slice(8*i++,8*i).padEnd(8,0)}`)))()
-}={}}={})=>(w=>((w.slice(w=w.pop()&0xf,w+4).reduce((a,x)=>a<<8|x)&0x7fffffff)+'').slice(-l))(
+totp=async({
+	_:{
+		be4=x=>[x>>>24&255,x>>>16&255,x>>>8&255,x>>>0&255],
+		b32de=w=>((
+			d=[...'abcdefghijklmnopqrstuvwxyz234567'].reduce((a,x,i)=>(a[x]=i.toString(2).padStart(5,0),a),{}),
+			b=w.toLowerCase().replace(/./g,x=>d[x]||'')
+		)=>new Uint8Array([...Array(Math.ceil(b.length/8))].map((_,i)=>+`0b${b.slice(8*i++,8*i).padEnd(8,0)}`)))()
+	}={},
+	date:t=new Date,algorithm:h='SHA-1',digits:l=6,period:d=30,secret:{base32:_k='',raw:k=b32de(_k)}
+}={})=>(w=>((w.slice(w=w.pop()&0xf,w+4).reduce((a,x)=>a<<8|x)&0x7fffffff)+'').slice(-l))(
 	[...new Uint8Array(await crypto.subtle.sign('HMAC',
-		await crypto.subtle.importKey('raw',b32de(k),{name:'HMAC',hash:h},0,['sign']),
+		await crypto.subtle.importKey('raw',k,{name:'HMAC',hash:h},0,['sign']),
 		new Uint8Array([...be4((t=t.getTime()/1000/d)/2**32),...be4(t)])
 	))]
 ),
@@ -34,14 +37,6 @@ migurl=w=>((
 
 export{totp};
 
-console.log(await Promise.all(migurl(
+console.log(await migurl(
 	'otpauth-migration://offline?data=CjkKCjkWt1nRWPW%2Bd98SEGhlbGxvd29ybGQyMzQ1NjcgASgBMAJCEzlkZDNjMzE3MzI3MjAwMzcyMTIQAhgBIAA%3D'
-).params.map(async x=>({
-	name:x.name,
-	issuer:x.issuer,
-	key:await totp({
-		secret:x.secret.base32,
-		algorithm:x.algorithm,
-		digits:x.digits
-	})
-}))));
+).params.reduce(async(a,x)=>(x.key=await totp(x),[...await a,x]),[]));
