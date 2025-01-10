e2c8f2fb73d801873d9b11a5d6949dd11d00618b
McbeEringi <git@mcbeeringi.dev>
2025-01-10 20:41:40 +0900

    -

--- png.mjs
+++ png.mjs
@@ -6,11 +6,11 @@ png=({data:d,width:w,height:h,palette:p,alpha:a})=>((
 	map=(x,f,n=65535)=>[...Array(Math.ceil(x.length/n))].flatMap((_,i,{length:l})=>f(x.slice(n*i,n*i+n),i,l))
 )=>Object.assign([
 	137,80,78,71,13,10,26,10,// header
-	...ch([73,72,68,82, ...be4(w),...be4(h), bd,p?3:alpha?6:2, 0,0,0]),// IHDR: w h bitDepth colType 0,0,0
+	...ch([73,72,68,82, ...be4(w),...be4(h), bd,p?3:a?6:2, 0,0,0]),// IHDR: w h bitDepth colType 0,0,0
 	...p?ch([80,76,84,69,...p.flatMap(x=>be4(a?x>>>8:x).slice(1))]):[],// PLTE: ...RGB
 	...p&&a?ch([116,82,78,83,...p.map(x=>x&255)]):[],// tRNS: ...alpha
-	...ch([73,68,65,84, 8,29, ...(x=>[...map(x,(y,i,a,l=y.length)=>[i==a-1,l>>>0&255,l>>>8&255,~l>>>0&255,~l>>>8&255,...y]),...be4(adler(x))])(
-		[...Array(h)].flatMap((_,i)=>(i=d.slice(w*i,w*++i),[0,...p?[...Array(Math.ceil(w/bdi))].map((_,j)=>[...Object.assign(i.slice(bdi*j,bdi*++j),{length:bdi})].reduce((a,x)=>a<<bd|(x&2**bd-1),0)):i]))
+	...ch([73,68,65,84, 8,29, ...(x=>[...map(x,(y,i,a,l=y.length)=>[+(i==a-1),l>>>0&255,l>>>8&255,~l>>>0&255,~l>>>8&255,...y]),...be4(adler(x))])(
+		[...Array(h)].flatMap((_,i)=>(i=d.slice(w*i,w*++i),[0,...p?[...Array(Math.ceil(w/bdi))].map((_,j)=>[...Object.assign(i.slice(bdi*j,bdi*++j),{length:bdi})].reduce((a,x)=>a<<bd|(x&2**bd-1),0)):i.flat()]))
 	)]),// DATA
 	0,0,0,0,73,69,78,68,174,66,96,130// IEND
 ],{toDataURL(){return'data:image/png;base64,'+btoa(map(this,x=>String.fromCharCode(...x)).join(''));},toBlob(){return new Blob([new Uint8Array(this)],{type:'image/png'})}}))();
