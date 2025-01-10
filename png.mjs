00907f76c920c163ed30057432c2b3ff11405ec2
McbeEringi <git@mcbeeringi.dev>
2025-01-10 21:23:33 +0900

    -

--- png.mjs
+++ png.mjs
@@ -10,7 +10,10 @@ png=({data:d,width:w,height:h,palette:p,alpha:a})=>((
 	...p?ch([80,76,84,69,...p.flatMap(x=>be4(a?x>>>8:x).slice(1))]):[],// PLTE: ...RGB
 	...p&&a?ch([116,82,78,83,...p.map(x=>x&255)]):[],// tRNS: ...alpha
 	...ch([73,68,65,84, 8,29, ...(x=>[...map(x,(y,i,a,l=y.length)=>[+(i==a-1),l>>>0&255,l>>>8&255,~l>>>0&255,~l>>>8&255,...y]),...be4(adler(x))])(
-		[...Array(h)].flatMap((_,i)=>(i=d.slice(w*i,w*++i),[0,...p?[...Array(Math.ceil(w/bdi))].map((_,j)=>[...Object.assign(i.slice(bdi*j,bdi*++j),{length:bdi})].reduce((a,x)=>a<<bd|(x&2**bd-1),0)):i.flat()]))
+		[...Array(h)].flatMap((_,i)=>(i=d.slice(w*i,w*++i),[0,...p?
+			[...Array(Math.ceil(w/bdi))].map((_,j)=>[...Object.assign(i.slice(bdi*j,bdi*++j),{length:bdi})].reduce((a,x)=>a<<bd|(x&2**bd-1),0)):
+			i.flatMap(x=>[...Array(a?4:3)].map((_,i,{length:l})=>x>>>(l-i-1)*8&255))
+		]))
 	)]),// DATA
 	0,0,0,0,73,69,78,68,174,66,96,130// IEND
 ],{toDataURL(){return'data:image/png;base64,'+btoa(map(this,x=>String.fromCharCode(...x)).join(''));},toBlob(){return new Blob([new Uint8Array(this)],{type:'image/png'})}}))();
