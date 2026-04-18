37df805013f5e4ef81506f574c574882d6879638
McbeEringi <git@mcbeeringi.dev>
2026-04-19 05:18:07 +0900

    -

--- sjis.mjs
+++ sjis.mjs
@@ -3,7 +3,7 @@ sjis=class{
 	constructor(){
 		this.e={};this.d={};
 		((td=new TextDecoder('sjis'),f=w=>(_=>
-			_.length==1&&_!='�'&&(this.e[_]=w).reduce((a,x,i,{length:l})=>(i==l-1?a[x]=_:a[x]||(a[x]={})),this.d)
+			_.length==1&&_!='\ufffd'&&(this.e[_]||(this.e[_]=w),w).reduce((a,x,i,{length:l})=>(i==l-1?a[x]=_:a[x]||(a[x]={})),this.d)
 		)(td.decode(new Uint8Array(w))))=>[...Array(238)].forEach((_,i)=>
 			i<191?f([i+(i<128?0:0xa1-128)]):[...Array(188)].forEach((_,j)=>f([i-191+(i-191<31?0x81:0xe0-31),j+0x40+(62<j)]))
 		))();
